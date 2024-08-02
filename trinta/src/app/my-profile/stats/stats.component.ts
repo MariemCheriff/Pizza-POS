fe6f97e9da66b2683e0da926469b2d9da2aa36ce
MariemCheriff <164797732+MariemCheriff@users.noreply.github.com>
2024-08-02 12:59:46 +0100

    l

--- trinta/src/app/my-profile/stats/stats.component.ts
+++ trinta/src/app/my-profile/stats/stats.component.ts
@@ -1,4 +1,5 @@
-import { Component, ViewChild, AfterViewInit } from '@angular/core';
+import { Component, ViewChild, AfterViewInit, Renderer2 } from '@angular/core';
+import { Router } from '@angular/router';
 import { RouterLink } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { MatSelectModule } from '@angular/material/select';
@@ -11,7 +12,6 @@ import { FeathericonsModule } from '../../icons/feathericons/feathericons.module
 import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
 import { MatTableModule, MatTableDataSource } from '@angular/material/table';
 import { FormsModule } from '@angular/forms';
-
 
 @Component({
   selector: 'app-stats',
@@ -26,15 +26,16 @@ import { FormsModule } from '@angular/forms';
     FeathericonsModule,
     MatDatepickerModule,
     MatSelectModule,
+    MatPaginatorModule,
+    MatTableModule,
     FormsModule
   ],
   templateUrl: './stats.component.html',
   styleUrls: ['./stats.component.scss']
 })
 export class StatsComponent implements AfterViewInit {
-  @ViewChild('confirmationModal', { static: true }) confirmationModal: any;
-  @ViewChild('usersClockedInModal', { static: true }) usersClockedInModal: any;
-
+  showMessage: boolean = false;
+  clockOutMessage: string = '';
   searchTerm: string = '';
   searchTerm1: string = '';
   selectedUser: string = '';
@@ -53,6 +54,115 @@ export class StatsComponent implements AfterViewInit {
     // Ajoutez d'autres transactions ici
   ];
   filteredTransactions = this.transactions;
+
+  users = [
+    { id: 1, username: 'Adam K', firstName: 'Adam', lastName: 'K', Due: 22 },
+    { id: 2, username: 'Manuel M', firstName: 'Manuel', lastName: 'M', Due: 67 },
+    { id: 3, username: 'Ciser C', firstName: 'Ciser', lastName: 'C', Due: 70 },
+    { id: 4, username: 'Sara P', firstName: 'Sara', lastName: 'P', Due: 92 },
+    { id: 5, username: 'Juthi M', firstName: 'Juthi', lastName: 'M', Due: 28 }
+  ];
+
+  selectedUserId: number | null = null;
+  selectedUsername: string | null = null;
+  selectedFirstName: string | null = null;
+  selectedLastName: string | null = null;
+  selectedDue: number | null = null;
+
+  displayedColumns: string[] = ['tickets', 'orderType', 'server', 'due', 'total', 'priority', 'action'];
+  dataSource = new MatTableDataSource(this.tickets);
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
+  constructor(private renderer: Renderer2, private router: Router) {}
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
+
+  closeConfirmationModal() {
+    const confirmationModalElement = document.getElementById('confirmationModal');
+    if (confirmationModalElement) {
+      this.renderer.removeClass(confirmationModalElement, 'show');
+      this.renderer.setStyle(confirmationModalElement, 'display', 'none');
+      this.renderer.setAttribute(confirmationModalElement, 'aria-hidden', 'true');
+      this.renderer.removeStyle(document.body, 'overflow');
+      this.renderer.removeStyle(document.body, 'padding-right');
+    }
+  }
+
+  clockOutUser() {
+    this.showMessage = true;
+    this.clockOutMessage = 'User is clocked out';
+
+    this.closeConfirmationModal();
+    this.openModal('usersClockedInModal2');
+  }
+
+  confirmClockOut() {
+    // Action à effectuer lors de la confirmation de la déconnexion
+    this.closeModal('usersClockedInModal2');
+    // Ajoutez ici toute autre logique nécessaire
+  }
+
+  openModal(modalId: string) {
+    const modalElement = document.getElementById(modalId);
+    if (modalElement) {
+      modalElement.classList.add('show');
+      this.renderer.setStyle(modalElement, 'display', 'block');
+      this.renderer.setAttribute(modalElement, 'aria-hidden', 'false');
+      this.renderer.setStyle(document.body, 'overflow', 'hidden');
+      this.renderer.setStyle(document.body, 'padding-right', '15px');
+    }
+  }
+
+  closeModal(modalId: string) {
+    const modalElement = document.getElementById(modalId);
+    if (modalElement) {
+      modalElement.classList.remove('show');
+      this.renderer.setStyle(modalElement, 'display', 'none');
+      this.renderer.setAttribute(modalElement, 'aria-hidden', 'true');
+      // Supprime le backdrop si nécessaire
+      const backdropElement = document.querySelector('.modal-backdrop');
+      if (backdropElement) {
+        backdropElement.remove();
+      }
+    }
+  }
+
+  closeAllModals() {
+    const modals = document.querySelectorAll('.modal.show');
+    modals.forEach(modal => {
+      const modalElement = modal as HTMLElement;
+      modalElement.classList.remove('show');
+      this.renderer.setStyle(modalElement, 'display', 'none');
+      this.renderer.setAttribute(modalElement, 'aria-hidden', 'true');
+    });
+    this.renderer.removeStyle(document.body, 'overflow');
+    this.renderer.removeStyle(document.body, 'padding-right');
+
+    // Supprime le backdrop si nécessaire
+    const backdropElement = document.querySelector('.modal-backdrop');
+    if (backdropElement) {
+      backdropElement.remove();
+    }
+  }
+
+  printMessage() {
+    const printContent = document.querySelector('#usersClockedInModal2 .alert');
+    if (printContent) {
+      const printWindow = window.open('', '', 'height=600,width=800');
+      if (printWindow) {
+        printWindow.document.open();
+        printWindow.document.write('<html><head><title>Print</title></head><body >');
+        printWindow.document.write(printContent.innerHTML);
+        printWindow.document.write('</body></html>');
+        printWindow.document.close();
+        printWindow.focus();
+        printWindow.print();
+      }
+    }
+  }
 
   searchTickets() {
     if (this.searchTerm.trim() === '') {
@@ -85,28 +195,9 @@ export class StatsComponent implements AfterViewInit {
     this.filteredTransactions = this.transactions;
   }
 
-
-
-  users = [
-    { id: 1, username: 'Adam K', firstName: 'Adam', lastName: 'K', Due: 22 },
-    { id: 2, username: 'Manuel M', firstName: 'Manuel', lastName: 'M', Due: 67 },
-    { id: 3, username: 'Ciser C', firstName: 'Ciser', lastName: 'C', Due: 70 },
-    { id: 4, username: 'Sara P', firstName: 'Sara', lastName: 'P', Due: 92 },
-    { id: 5, username: 'Juthi M', firstName: 'Juthi', lastName: 'M', Due: 28 }
-  ];
-
-  selectedUserId: number | null = null;
-  selectedUsername: string | null = null;
-  selectedFirstName: string | null = null;
-  selectedLastName: string | null = null;
-  selectedDue: number | null = null;
-
   get totalDue(): number {
     return this.users.reduce((total, user) => total + user.Due, 0);
   }
-
-  constructor() { }
-
 
   onUserSelect(event: Event): void {
     const selectElement = event.target as HTMLSelectElement;
@@ -144,22 +235,41 @@ export class StatsComponent implements AfterViewInit {
     }
   }
 
-
-  displayedColumns: string[] = ['tickets', 'orderType', 'server', 'due', 'total', 'priority', 'action'];
-  dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
-
-  @ViewChild(MatPaginator) paginator!: MatPaginator;
-
-  ngAfterViewInit() {
-    this.dataSource.paginator = this.paginator;
-  }
-
   // Search Filter
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
     this.dataSource.filter = filterValue.trim().toLowerCase();
   }
+
+  redirectToMyProfile() {
+    this.closeAllModals(); // Fermer tous les modals ouverts
+    this.router.navigate(['./my-profile']); // Rediriger vers ./my-profile
+  }
+
+  // Nouvelles fonctions pour storestatusModal
+
+  generateReport() {
+    // Logique pour générer un rapport
+    console.log('Generating report...');
+    // Ajoutez votre logique pour générer un rapport ici
+  }
+
+  viewHistory() {
+    // Logique pour afficher l'historique
+    console.log('Viewing history...');
+    // Ajoutez votre logique pour afficher l'historique ici
+  }
+
+  closeStore() {
+    // Logique pour fermer le magasin
+    console.log('Closing store...');
+    // Ajoutez votre logique pour fermer le magasin ici
+  }
 }
+
+
+
+
 
 export interface PeriodicElement {
   taskName: string;
